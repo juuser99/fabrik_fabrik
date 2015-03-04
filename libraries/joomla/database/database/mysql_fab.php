41238282a52a3d5c4a33b0f80fa39986b84289d3
Hugh Messenger <hugh.messenger@gmail.com>
2015-03-03 22:11:22 -0600

    Fix for replacePrefix

--- libraries/joomla/database/database/mysql_fab.php
+++ libraries/joomla/database/database/mysql_fab.php
@@ -36,7 +36,7 @@ class JDatabaseMySQL_Fab extends JDatabaseMySQL
 	public function replacePrefix($sql, $prefix='#__')
 	{
 		$app = JFactory::getApplication();
-		$package = $app->getUserStateFromRequest('com_fabrik.package', 'package', 'fabrik');
+		$package = $app->getUserStateFromRequest('com_fabrik.package', 'package', 'fabrik', 'cmd');
 		$sql = str_replace('{package}', $package, $sql);
 		return parent::replacePrefix($sql, $prefix);
 	}
