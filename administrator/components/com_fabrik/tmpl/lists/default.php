2114d25e0b6fb6ff1d3afcb87625e84ec1d6022f
Alan Hartless <alan@devkardia.com>
2018-11-19 21:50:54 -0600

    Fixed div layout

--- administrator/components/com_fabrik/tmpl/lists/default.php
+++ administrator/components/com_fabrik/tmpl/lists/default.php
@@ -34,118 +34,115 @@ $listDirn  = $this->state->get('list.direction');
         <div id="j-sidebar-container" class="col-2">
 			<?php echo $this->sidebar; ?>
         </div>
+        <div id="j-main-container" class="col-10">
+			<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 
-        <div class="col-10">
-            <div id="j-main-container" class="j-main-container">
-				<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
-                <div class="clearfix"> </div>
-
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th width="2%">
-							<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 'l.id', $listDirn, $listOrder); ?>
-                        </th>
-                        <th width="1%">
-	                        <?php echo HTMLHelper::_('grid.checkall'); ?>
-                        </th>
-                        <th width="16%">
-							<?php echo HTMLHelper::_('searchtools.sort', 'COM_FABRIK_LIST_NAME', 'label', $listDirn, $listOrder); ?>
-                        </th>
-                        <th width="17%">
-							<?php echo HTMLHelper::_('searchtools.sort', 'COM_FABRIK_DB_TABLE_NAME', 'db_table_name', $listDirn, $listOrder); ?>
-                        </th>
-                        <th width="14%">
-							<?php echo Text::_('COM_FABRIK_ELEMENT');?>
-                        </th>
-                        <th width="14%">
-							<?php echo Text::_('COM_FABRIK_FORM'); ?>
-                        </th>
-                        <th width="16%">
-							<?php echo Text::_('COM_FABRIK_VIEW_DATA');?>
-                        </th>
-                        <th width="20%">
-							<?php echo Text::_('COM_FABRIK_VIEW_DETAILS'); ?>
-                        </th>
-                        <th width="5%">
-							<?php echo HTMLHelper::_('searchtools.sort', 'JPUBLISHED', 'published', $listDirn, $listOrder); ?>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tfoot>
-                    <tr>
-                        <td colspan="9">
-							<?php echo $this->pagination->getListFooter(); ?>
+            <div class="clearfix"></div>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th width="2%">
+                        <?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 'l.id', $listDirn, $listOrder); ?>
+                    </th>
+                    <th width="1%">
+                        <?php echo HTMLHelper::_('grid.checkall'); ?>
+                    </th>
+                    <th width="16%">
+                        <?php echo HTMLHelper::_('searchtools.sort', 'COM_FABRIK_LIST_NAME', 'label', $listDirn, $listOrder); ?>
+                    </th>
+                    <th width="17%">
+                        <?php echo HTMLHelper::_('searchtools.sort', 'COM_FABRIK_DB_TABLE_NAME', 'db_table_name', $listDirn, $listOrder); ?>
+                    </th>
+                    <th width="14%">
+                        <?php echo Text::_('COM_FABRIK_ELEMENT');?>
+                    </th>
+                    <th width="14%">
+                        <?php echo Text::_('COM_FABRIK_FORM'); ?>
+                    </th>
+                    <th width="16%">
+                        <?php echo Text::_('COM_FABRIK_VIEW_DATA');?>
+                    </th>
+                    <th width="20%">
+                        <?php echo Text::_('COM_FABRIK_VIEW_DETAILS'); ?>
+                    </th>
+                    <th width="5%">
+                        <?php echo HTMLHelper::_('searchtools.sort', 'JPUBLISHED', 'published', $listDirn, $listOrder); ?>
+                    </th>
+                </tr>
+                </thead>
+                <tfoot>
+                <tr>
+                    <td colspan="9">
+                        <?php echo $this->pagination->getListFooter(); ?>
+                    </td>
+                </tr>
+                </tfoot>
+                <tbody>
+                <?php foreach ($this->items as $i => $item) :
+                    $ordering = ($listOrder == 'ordering');
+                    $link = Route::_('index.php?option=com_fabrik&task=list.edit&id=' . $item->id);
+                    $params = new Registry($item->params);
+                    $elementLink = Route::_('index.php?option=com_fabrik&task=element.edit&id=0&filter_groupId=' . $this->tableGroups[$item->id]->group_id);
+                    $formLink = Route::_('index.php?option=com_fabrik&task=form.edit&id=' . $item->form_id);
+                    $canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
+                    $canChange = true;
+                    ?>
+                    <tr class="row<?php echo $i % 2; ?>">
+                        <td>
+                            <?php echo $item->id; ?>
+                        </td>
+                        <td><?php echo HTMLHelper::_('searchtools.id', $i, $item->id); ?></td>
+                        <td>
+                            <?php if ($item->checked_out) : ?>
+                                <?php echo HTMLHelper::_('jsearchtools.checkedout', $i, $item->editor, $item->checked_out_time, 'lists.', $canCheckin); ?>
+                            <?php endif; ?>
+                            <?php
+                            if ($item->checked_out && ( $item->checked_out != $user->get('id'))) : ?>
+                                <span class="editlinktip hasTip"
+                                      title="foo <?php echo Text::_($item->label) . "::" . $params->get('note'); ?>"> <?php echo $item->label; ?>
+                            </span>
+                            <?php else : ?>
+                                <a href="<?php echo $link;?>">
+                                <span class="editlinktip hasTip" title="<?php echo $item->label . "::" . $params->get('note'); ?>">
+                                    <?php echo Text::_($item->label); ?>
+                                </span>
+                                </a>
+                            <?php endif; ?>
+                        </td>
+                        <td>
+                            <?php echo $item->db_table_name;?>
+                        </td>
+                        <td>
+                            <a href="<?php echo $elementLink?>">
+                                <i class="icon-plus"></i> <?php echo Text::_('COM_FABRIK_ADD');?>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="<?php echo $formLink; ?>">
+                                <i class="icon-pencil"></i> <?php echo Text::_('COM_FABRIK_EDIT'); ?>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="index.php?option=com_fabrik&task=list.view&listid=<?php echo $item->id;?>">
+                                <i class="icon-list-view"></i> <?php echo Text::_('COM_FABRIK_VIEW_DATA');?>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="#showlinkedelements" onclick="return listItemTask('cb<?php echo $i;?>','list.showLinkedElements');">
+                                <?php echo Text::_('COM_FABRIK_VIEW_DETAILS');?>
+                            </a>
+                        </td>
+                        <td class="center">
+                            <?php echo HTMLHelper::_('jsearchtools.published', $item->published, $i, 'lists.', $canChange);?>
                         </td>
                     </tr>
-                    </tfoot>
-                    <tbody>
-					<?php foreach ($this->items as $i => $item) :
-						$ordering = ($listOrder == 'ordering');
-						$link = Route::_('index.php?option=com_fabrik&task=list.edit&id=' . $item->id);
-						$params = new Registry($item->params);
-						$elementLink = Route::_('index.php?option=com_fabrik&task=element.edit&id=0&filter_groupId=' . $this->tableGroups[$item->id]->group_id);
-						$formLink = Route::_('index.php?option=com_fabrik&task=form.edit&id=' . $item->form_id);
-						$canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
-						$canChange = true;
-						?>
-                        <tr class="row<?php echo $i % 2; ?>">
-                            <td>
-								<?php echo $item->id; ?>
-                            </td>
-                            <td><?php echo HTMLHelper::_('searchtools.id', $i, $item->id); ?></td>
-                            <td>
-								<?php if ($item->checked_out) : ?>
-									<?php echo HTMLHelper::_('jsearchtools.checkedout', $i, $item->editor, $item->checked_out_time, 'lists.', $canCheckin); ?>
-								<?php endif; ?>
-								<?php
-								if ($item->checked_out && ( $item->checked_out != $user->get('id'))) : ?>
-                                    <span class="editlinktip hasTip"
-                                          title="foo <?php echo Text::_($item->label) . "::" . $params->get('note'); ?>"> <?php echo $item->label; ?>
-                                </span>
-								<?php else : ?>
-                                    <a href="<?php echo $link;?>">
-                                    <span class="editlinktip hasTip" title="<?php echo $item->label . "::" . $params->get('note'); ?>">
-                                        <?php echo Text::_($item->label); ?>
-                                    </span>
-                                    </a>
-								<?php endif; ?>
-                            </td>
-                            <td>
-								<?php echo $item->db_table_name;?>
-                            </td>
-                            <td>
-                                <a href="<?php echo $elementLink?>">
-                                    <i class="icon-plus"></i> <?php echo Text::_('COM_FABRIK_ADD');?>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="<?php echo $formLink; ?>">
-                                    <i class="icon-pencil"></i> <?php echo Text::_('COM_FABRIK_EDIT'); ?>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="index.php?option=com_fabrik&task=list.view&listid=<?php echo $item->id;?>">
-                                    <i class="icon-list-view"></i> <?php echo Text::_('COM_FABRIK_VIEW_DATA');?>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="#showlinkedelements" onclick="return listItemTask('cb<?php echo $i;?>','list.showLinkedElements');">
-									<?php echo Text::_('COM_FABRIK_VIEW_DETAILS');?>
-                                </a>
-                            </td>
-                            <td class="center">
-								<?php echo HTMLHelper::_('jsearchtools.published', $item->published, $i, 'lists.', $canChange);?>
-                            </td>
-                        </tr>
-					<?php endforeach; ?>
-                    </tbody>
-                </table>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
 
-                <input type="hidden" name="task" value="" />
-                <input type="hidden" name="boxchecked" value="0" />
-				<?php echo HTMLHelper::_('form.token'); ?>
-            </div>
+            <input type="hidden" name="task" value="" />
+            <input type="hidden" name="boxchecked" value="0" />
+            <?php echo HTMLHelper::_('form.token'); ?>
         </div>
     </div>
 </form>
--- administrator/components/com_fabrik/tmpl/visualizations/default.php
+++ administrator/components/com_fabrik/tmpl/visualizations/default.php
@@ -31,77 +31,77 @@ $listDirn = $this->state->get('list.direction');
         <div id="j-sidebar-container" class="col-2">
 			<?php echo $this->sidebar; ?>
         </div>
+        <div id="j-main-container" class="col-10">
+			<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 
-        <div class="col-10">
-            <div id="j-main-container" class="j-main-container">
-				<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
-                <div class="clearfix"> </div>
+            <div class="clearfix"></div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th width="2%"><?php echo HTMLHelper::_('grid.sort',  'JGRID_HEADING_ID', 'v.id', $listDirn, $listOrder); ?></th>
+                        <th width="1%"> <input type="checkbox" name="toggle" value="" onclick="Joomla.checkAll(this)" /> </th>
+                        <th width="35%" >
+                            <?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_LABEL', 'v.label', $listDirn, $listOrder); ?>
+                        </th>
+                        <th width="35%">
+                            <?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_TYPE', 'v.plugin', $listDirn, $listOrder); ?>
+                        </th>
+                        <th width="5%">
+                            <?php echo HTMLHelper::_('grid.sort', 'JPUBLISHED', 'v.published', $listDirn, $listOrder); ?>
+                        </th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <td colspan="5">
+                            <?php echo $this->pagination->getListFooter(); ?>
+                        </td>
+                    </tr>
+                </tfoot>
+                <tbody>
+                <?php foreach ($this->items as $i => $item) :
+                    $ordering = ($listOrder == 'ordering');
+                    $link = Route::_('index.php?option=com_fabrik&task=visualization.edit&id=' . (int) $item->id);
+                    $canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
+                    $canChange = true;
+                    ?>
 
-                <table class="table table-striped">
-		<thead>
-			<tr>
-				<th width="2%"><?php echo HTMLHelper::_('grid.sort',  'JGRID_HEADING_ID', 'v.id', $listDirn, $listOrder); ?></th>
-				<th width="1%"> <input type="checkbox" name="toggle" value="" onclick="Joomla.checkAll(this)" /> </th>
-				<th width="35%" >
-					<?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_LABEL', 'v.label', $listDirn, $listOrder); ?>
-				</th>
-				<th width="35%">
-					<?php echo HTMLHelper::_('grid.sort', 'COM_FABRIK_TYPE', 'v.plugin', $listDirn, $listOrder); ?>
-				</th>
-				<th width="5%">
-					<?php echo HTMLHelper::_('grid.sort', 'JPUBLISHED', 'v.published', $listDirn, $listOrder); ?>
-				</th>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td colspan="5">
-					<?php echo $this->pagination->getListFooter(); ?>
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-		<?php foreach ($this->items as $i => $item) :
-			$ordering = ($listOrder == 'ordering');
-			$link = Route::_('index.php?option=com_fabrik&task=visualization.edit&id=' . (int) $item->id);
-			$canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
-			$canChange = true;
-			?>
+                    <tr class="row<?php echo $i % 2; ?>">
+                            <td>
+                                <?php echo $item->id; ?>
+                            </td>
+                            <td>
+                                <?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
+                            </td>
+                            <td>
+                                <?php if ($item->checked_out) : ?>
+                                    <?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'visualizations.', $canCheckin); ?>
+                                <?php endif; ?>
+                                <?php
+                                if ($item->checked_out && ( $item->checked_out != $user->get('id'))) :
+                                    echo $item->label;
+                                else:
+                                ?>
+                                <a href="<?php echo $link; ?>">
+                                    <?php echo $item->label; ?>
+                                </a>
+                            <?php endif; ?>
+                            </td>
+                            <td>
+                                <?php echo $item->plugin; ?>
+                            </td>
+                            <td class="center">
+                                <?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'visualizations.', $canChange); ?>
+                            </td>
+                        </tr>
 
-			<tr class="row<?php echo $i % 2; ?>">
-					<td>
-						<?php echo $item->id; ?>
-					</td>
-					<td>
-						<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
-					</td>
-					<td>
-						<?php if ($item->checked_out) : ?>
-							<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'visualizations.', $canCheckin); ?>
-						<?php endif; ?>
-						<?php
-						if ($item->checked_out && ( $item->checked_out != $user->get('id'))) :
-							echo $item->label;
-						else:
-						?>
-						<a href="<?php echo $link; ?>">
-							<?php echo $item->label; ?>
-						</a>
-					<?php endif; ?>
-					</td>
-					<td>
-						<?php echo $item->plugin; ?>
-					</td>
-					<td class="center">
-						<?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'visualizations.', $canChange); ?>
-					</td>
-				</tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
 
-			<?php endforeach; ?>
-		</tbody>
-	</table>
-	<input type="hidden" name="task" value="" />
-	<input type="hidden" name="boxchecked" value="0" />
-	<?php echo HTMLHelper::_('form.token'); ?>
+        <input type="hidden" name="task" value="" />
+        <input type="hidden" name="boxchecked" value="0" />
+        <?php echo HTMLHelper::_('form.token'); ?>
 	</div>
 </form>
