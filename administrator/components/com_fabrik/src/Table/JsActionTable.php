ae440b7261db11f136aeca97279379cff2434976
Alan Hartless <alan@devkardia.com>
2019-01-02 12:45:31 -0600

    Refactor JsActionTable used in FE

---
+++ administrator/components/com_fabrik/src/Table/JsActionTable.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * JS Action Fabrik table
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+namespace Joomla\Component\Fabrik\Administrator\Table;
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\Database\DatabaseDriver;
+
+/**
+ * JS Action Fabrik table
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @since       4.0
+ */
+class JsActionTable extends FabTable
+{
+	/**
+	 * JsActionTable constructor.
+	 *
+	 * @param DatabaseDriver $db
+	 *
+	 * @since 4.0
+	 */
+	public function __construct(DatabaseDriver $db)
+	{
+		parent::__construct('#__{package}_jsactions', 'id', $db);
+	}
+}
