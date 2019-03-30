54cbfc4bb9ee1039b536292387a62913072a358d
Alan Hartless <alan@devkardia.com>
2019-03-30 06:22:14 -0500

    Port display method from FE legacy controller.php

--- components/com_fabrik/src/Controller/AbstractSiteController.php
+++ components/com_fabrik/src/Controller/AbstractSiteController.php
@@ -14,12 +14,15 @@ namespace Fabrik\Component\Fabrik\Site\Controller;
 defined('_JEXEC') or die('Restricted access');
 
 use Fabrik\Component\Fabrik\Administrator\Controller\ModelTrait;
+use Fabrik\Helpers\Html;
+use Fabrik\Helpers\Worker;
 use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Document\Document;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\CMS\Session\Session;
+use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\User\User;
 use Joomla\Database\DatabaseDriver;
 use Joomla\Registry\Registry;
@@ -28,6 +31,24 @@ use Joomla\Utilities\ArrayHelper;
 class AbstractSiteController extends BaseController
 {
 	use ModelTrait;
+
+	/**
+	 * Is the controller inside a content plug-in
+	 *
+	 * @var  bool
+	 *
+	 * @since 4.0
+	 */
+	public $isMambot = false;
+
+	/**
+	 * Id used from content plugin when caching turned on to ensure correct element rendered)
+	 *
+	 * @var  int
+	 *
+	 * @since 4.0
+	 */
+	public $cacheId = 0;
 
 	/**
 	 * @var User
@@ -100,6 +121,78 @@ class AbstractSiteController extends BaseController
 		$this->config  = ArrayHelper::getValue($config, 'config', $this->app->getConfig());
 
 		parent::__construct($config, $factory, $this->app, $input);
+	}
+
+	/**
+	 * Display the view
+	 *
+	 * @param bool  $cachable  If true, the view output will be cached - NOTE not actually used to control caching!!!
+	 * @param array $urlparams An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+	 *
+	 * @return  null
+	 * @since 4.0
+	 *
+	 */
+	public function display($cachable = false, $urlparams = false)
+	{
+		/** @var CMSApplication $app */
+		$app     = Factory::getApplication();
+		$input   = $app->input;
+		$package = $app->getUserState('com_fabrik.package', 'fabrik');
+
+		// Menu links use fabriklayout parameters rather than layout
+		$flayout = $input->get('fabriklayout');
+
+		if ($flayout != '')
+		{
+			$input->set('layout', $flayout);
+		}
+
+		$document = $app->getDocument();
+
+		$viewName  = $input->get('view', 'form');
+		$modelName = $viewName;
+
+		if ($viewName == 'emailform')
+		{
+			$modelName = 'form';
+		}
+
+		if ($viewName == 'details')
+		{
+			$viewName  = 'form';
+			$modelName = 'form';
+		}
+
+		$viewType = $document->getType();
+
+		// Set the default view name from the Request
+		$view = $this->getView($viewName, $viewType);
+
+		// Push a model into the view
+		if ($model = $this->getModel($modelName))
+		{
+			$view->setModel($model, true);
+		}
+
+		// Display the view
+
+		$view->error = $this->getError();
+
+		if (Worker::useCache() && !$this->isMambot)
+		{
+			$user    = Factory::getUser();
+			$uri     = Uri::getInstance();
+			$uri     = $uri->toString(array('path', 'query'));
+			$cacheid = serialize(array($uri, $input->post, $user->get('id'), get_class($view), 'display', $this->cacheId));
+			$cache   = Factory::getCache('com_' . $package, 'view');
+			Html::addToSessionCacheIds($this->cacheId);
+			echo $cache->get($view, 'display', $cacheid);
+		}
+		else
+		{
+			return $view->display();
+		}
 	}
 
 	/**
