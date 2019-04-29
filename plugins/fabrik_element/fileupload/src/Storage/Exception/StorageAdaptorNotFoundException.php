90f3b91534ec35540beff11656f2ccd318324acb
Alan Hartless <alan@devkardia.com>
2019-04-28 19:39:12 -0500

    Fixed exception message

--- plugins/fabrik_element/fileupload/src/Storage/Exception/StorageAdaptorNotFoundException.php
+++ plugins/fabrik_element/fileupload/src/Storage/Exception/StorageAdaptorNotFoundException.php
@@ -2,20 +2,17 @@
 /**
  * @package     Joomla
  * @subpackage  Fabrik
- * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @copyright   Copyright (C) 2005-2019  Media A-Team, Inc. - All rights reserved.
  * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
 
 namespace Fabrik\Plugin\FabrikElement\Fileupload\Storage\Exception;
 
 
-use Throwable;
-
 class StorageAdaptorNotFoundException extends \Exception
 {
 	public function __construct($alias)
 	{
-
-		parent::__construct($message, $code, $previous);
+		parent::__construct("$alias is not registered as a storage adaptor");
 	}
 }
